3f103ed8770581b97013c2792a6639542f20c4e7
Ben Pomerenke <pomerenb@asynchrony.com>
2017-07-25 08:09:25 -0500

    cleanup data for presentation - BP

--- api/Controllers/PtoUsageController.cs
+++ api/Controllers/PtoUsageController.cs
@@ -10,21 +10,19 @@ namespace api.Controllers
     public class PtoUsageController : Controller
     {
         public static List<PtoUsage> ptoUsages = new List<PtoUsage> {
-            new PtoUsage {id= 1, startDate= "1-13-17", endDate= "1-13-17", hoursUsed= 4, title= "???"},
-            new PtoUsage{id= 2, startDate= "1-27-17", endDate= "1-27-17", hoursUsed= 2, title= "???"},
-            new PtoUsage{id= 3, startDate= "2-10-17", endDate= "2-10-17", hoursUsed= 1, title= "???"},
-            new PtoUsage{id= 4, startDate= "2-15-17", endDate= "2-15-17", hoursUsed= 1, title= "???"},
-            new PtoUsage{id= 5, startDate= "3-13-17", endDate= "3-13-17", hoursUsed= 1, title= "???"},
-            new PtoUsage{id= 6, startDate= "3-20-17", endDate= "3-28-17", hoursUsed= 39, title= "ski trip"},
-            new PtoUsage{id= 7, startDate= "3-31-17", endDate= "4-07-17", hoursUsed= 2, title= "???"}, 
-            new PtoUsage{id= 8, startDate= "4-28-17", endDate= "4-28-17", hoursUsed= 3, title= "holly wedding"}, 
-            new PtoUsage{id= 5, startDate= "5-12-17", endDate= "5-31-17", hoursUsed= 12, title= "???"},
-            new PtoUsage{id= 9, startDate= "6-1-17", endDate= "6-5-17", hoursUsed= 20, title= "john wedding / DC"},
-            new PtoUsage{id= 9, startDate= "6-23-17", endDate= "6-23-17", hoursUsed= 8, title= "durham bach party"},
-            new PtoUsage{id= 9, startDate= "7-7-17", endDate= "7-7-17", hoursUsed= 8, title= "durham wedding"},
-            new PtoUsage{id= 10, startDate= "7-17-17", endDate= "7-21-17", hoursUsed= 40, title= "lake trip"},
-            new PtoUsage{id= 11, startDate= "8-18-17", endDate= "8-18-17", hoursUsed= 8, title= "nate wedding"}, 
-            new PtoUsage{id= 12, startDate= "12-25-17", endDate= "12-31-17", hoursUsed= 24, title= "holidays"}
+            new PtoUsage {id= 1, startDate= "1-13-17", endDate= "1-13-17", hoursUsed= 4, title= "Doctor"},
+            new PtoUsage{id= 2, startDate= "1-27-17", endDate= "1-27-17", hoursUsed= 2, title= "Leave Early"},
+            new PtoUsage{id= 3, startDate= "2-10-17", endDate= "2-10-17", hoursUsed= 2, title= "Leave Early (x2)"},
+            new PtoUsage{id= 6, startDate= "3-20-17", endDate= "3-28-17", hoursUsed= 39, title= "Ski Trip"},
+            new PtoUsage{id= 7, startDate= "3-31-17", endDate= "4-07-17", hoursUsed= 2, title= "Leave Early"}, 
+            new PtoUsage{id= 8, startDate= "4-28-17", endDate= "4-28-17", hoursUsed= 3, title= "Wedding"}, 
+            new PtoUsage{id= 5, startDate= "5-12-17", endDate= "5-31-17", hoursUsed= 12, title= "Misc"},
+            new PtoUsage{id= 9, startDate= "6-1-17", endDate= "6-5-17", hoursUsed= 20, title= "Wedding / DC"},
+            new PtoUsage{id= 9, startDate= "6-23-17", endDate= "6-23-17", hoursUsed= 8, title= "Bach Party"},
+            new PtoUsage{id= 9, startDate= "7-7-17", endDate= "7-7-17", hoursUsed= 8, title= "Wedding"},
+            new PtoUsage{id= 10, startDate= "7-17-17", endDate= "7-21-17", hoursUsed= 40, title= "Lake Trip"},
+            new PtoUsage{id= 11, startDate= "8-18-17", endDate= "8-18-17", hoursUsed= 8, title= "Wedding"}, 
+            new PtoUsage{id= 12, startDate= "12-25-17", endDate= "12-31-17", hoursUsed= 24, title= "Holidays"}
         };
 
         // GET api/ptousage
