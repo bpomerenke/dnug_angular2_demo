6b751c81b7986b595b15cf3a7f0c7e46d1ad9359
Ben Pomerenke <pomerenb@asynchrony.com>
2017-07-25 17:29:40 -0500

    fixed unit tests - BP

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,28 @@
 import { TestBed, async } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+
+import { ChartsModule } from 'ng2-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { PtoUsageListComponent } from './pto/pto-usage-list.component'
+import { PtoUsageChartComponent } from './pto/pto-usage-chart.component'
+import { PtoUsageService } from './services/pto-usage.service'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, 
+        PtoUsageListComponent,
+        PtoUsageChartComponent
       ],
+      imports: [
+        HttpModule,
+        ChartsModule,
+        NgbModule.forRoot()
+      ],
+      providers: [PtoUsageService],
     }).compileComponents();
   }));
 
@@ -17,16 +32,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('h1').textContent).toContain('Vaca Countdown');
   }));
 });
